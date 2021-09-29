0a230b7e103628d6aa6789cf79617619e5b147d1
Siddharth Joharwal <siddjoharwal@gmail.com>
2021-09-29 09:39:39 -0700

    firstCommit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+import java.util.*;
+import java.io.*;
+import java.util.stream.Collectors;
+
+class A{
+
+    public void printt(){
+        System.out.println("Hello World");
+    }
+}
+
+class B{
+    @Autowired
+    public A obj;
+    public void func(){
+        obj.printt();
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.func();
+    }
+}
